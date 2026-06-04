b35d0fdd8e20860802ee88ee0cf54327acda0711
PHAN TUAN <tuanpmhe187404@fpt.edu.vn>
2026-06-04 22:48:24 +0700

    New feature: add post, edit post, delete post

---
+++ src/test/java/org/example/group03/Group03ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.group03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group03ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
